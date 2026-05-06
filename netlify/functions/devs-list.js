0df3b24ad7734853ed394627fab62624c53d2278
ottonowmy <ottonowmy@gmail.com>
2026-05-06 17:40:31 +0200

    Add files via upload

--- netlify/functions/devs-list.js
+++ netlify/functions/devs-list.js
@@ -1,29 +1,19 @@
-export async function handler(event) {
+export async function handler() {
   try {
-    const body =
-      typeof event.body === "string"
-        ? JSON.parse(event.body)
-        : event.body;
-
     const response = await fetch(
       `https://api.airtable.com/v0/${process.env.BASE_ID}/Devs`,
       {
-        method: "POST",
         headers: {
           Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`,
-          "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          fields: body.fields,
-        }),
       }
     );
 
     const data = await response.json();
 
     return {
-      statusCode: response.status,
-      body: JSON.stringify(data),
+      statusCode: 200,
+      body: JSON.stringify(data.records),
     };
   } catch (err) {
     return {
